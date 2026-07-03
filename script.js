08536b5eca1ed2e6ec5494b697a74c233ca126ee
moizasif484 <mohsinasif051@gmail.com>
2026-07-03 11:08:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
-function hello(){
-alert("Welcome Mohsin Website!");
+const words = [
+    "Moiz Developer",
+    "Web Developer",
+    "Frontend Developer",
+    "AI Enthusiast"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let typingElement = document.getElementById("typing");
+
+function type() {
+
+    if(charIndex < words[wordIndex].length){
+
+        typingElement.innerHTML += words[wordIndex].charAt(charIndex);
+
+        charIndex++;
+
+        setTimeout(type,100);
+
+    }else{
+
+        setTimeout(erase,1500);
+
+    }
+
 }
+
+function erase(){
+
+    if(charIndex > 0){
+
+        typingElement.innerHTML = words[wordIndex].substring(0,charIndex-1);
+
+        charIndex--;
+
+        setTimeout(erase,50);
+
+    }else{
+
+        wordIndex++;
+
+        if(wordIndex >= words.length){
+            wordIndex = 0;
+        }
+
+        setTimeout(type,300);
+
+    }
+
+}
+
+type();
